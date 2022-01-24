e0a38aab40231c2ad5bd27ce8bb135209d7733b4
Bruno Marcenaro <bems2588@gmail.com>
2022-01-24 12:53:57 -0500

    fix robots name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,15 @@
+var getPlayerName = function() {
+  var name = "";
 
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
 
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -31,6 +39,7 @@ var playerInfo = {
     }
   }
 };
+
 
 
 
@@ -102,6 +111,7 @@ playerInfo.reset();
   for(var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0){
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+      debugger;
     
       var pickedEnemyObj = enemyInfo[i];
     
